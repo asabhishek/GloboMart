10a8bedff3cdd50521d8fa6f29da86a141a62c44
asabhishek <as.abhishek@gmail.com>
2016-02-12 10:53:27 +0530

    bootstraper an common

---
+++ GloboMart.Business.Bootstrapper/MEFLoader.cs
@@ -0,0 +1,19 @@
+﻿using GloboMart.Data;
+using System.ComponentModel.Composition.Hosting;
+using GloboMart.Business.Bootstrapper;
+namespace GloboMart.Business.Bootstrapper
+{
+    public static class MefLoader
+    {
+        
+        public static CompositionContainer Init()
+        {
+            AggregateCatalog catalog = new AggregateCatalog();
+
+            catalog.Catalogs.Add(new AssemblyCatalog(typeof(ProductRepository).Assembly));
+
+            CompositionContainer container = new CompositionContainer(catalog);
+            return container;
+        }
+    }
+}
